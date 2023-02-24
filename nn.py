01b28619cb0f5b206cfce689daffd337bab57704
seankwarren <seankwarren722@gmail.com>
2023-02-24 14:20:30 -0800

    Notebook updated with markdown

--- nn.py
+++ nn.py
@@ -1,40 +1,192 @@
+"""
+The `neural_networks` module provides classes for building multi-layer perceptron (MLP)
+neural networks.
+
+Classes:
+- `Neuron`: a class representing a single neuron in a neural network.
+- `Layer`: a class representing a layer of neurons in a neural network.
+- `MLP`: a class representing a multi-layer perceptron neural network.
+
+Each class provides a `parameters` method to retrieve the learnable parameters of the network or
+layer, as well as a `__call__` method for forward propagation through the network. The `MLP`
+class also provides a `zero_grad` method to reset gradients to zero between iterations during
+training.
+
+Example usage:
+
+import neural_networks
+
+# create a 2-layer MLP with 4 input neurons, 8 hidden neurons, and 1 output neuron
+mlp = neural_networks.MLP(4, [8, 1])
+
+# forward propagate through the network with some input data
+output = mlp([1.0, 2.0, 3.0, 4.0])
+
+# retrieve the learnable parameters of the network
+params = mlp.parameters()
+
+# reset gradients to 0 before running backpropagation
+mlp.zero_grad()
+
+# backpropagation
+mlp.backward()
+"""
 import random
 from value import Value
 
-class Neuron:
+class Module:
+
+    def zero_grad(self):
+        """
+        Resets the gradients to 0 for all parameters in the MLP.
+        """
+        for p in self.parameters():
+            # reset gradients to 0 between iterations
+            p.grad = 0.0
+
+    def parameters(self):
+        return []
+
+class Neuron(Module):
+    """
+    A single neuron with an arbitrary number of input connections.
+
+    Args:
+        nin (int): The number of input connections.
+
+    Attributes:
+        w (List[Value]): A list of input weights.
+        b (Value): The bias weight.
+
+    """
     def __init__(self, nin):
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
         self.b = Value(random.uniform(-1,1))
-    
+
     def __call__(self, x):
+        """
+        Compute the output of the neuron given an input.
+
+        Args:
+            x (List[Value]): A list of input values.
+
+        Returns:
+            Value: The output of the neuron.
+
+        """
         # w * x + b
         act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
         return act.tanh()
 
     def parameters(self):
-        return self.w + [self.b]
+        """
+        Get a list containing the weights and bias of the neuron.
 
-class Layer:
+        Returns:
+            List[Value]: A list of the weights and biases of the neuron.
+
+        """
+        return self.w + [self.b]
+    
+    def __repr__(self):
+        return f"Neuron({len(self.w)})"
+
+class Layer(Module):
+    """
+    A layer of neurons.
+
+    Args:
+        nin (int): The number of input connections for each neuron.
+        nout (int): The number of neurons in the layer.
+
+    Attributes:
+        neurons (List[Neuron]): A list of neurons in the layer.
+
+    """
     def __init__(self, nin, nout):
         self.neurons = [Neuron(nin) for _ in range(nout)]
     
     def __call__(self, x):
+        """
+        Compute the output of the layer given an input.
+
+        Args:
+            x (List[Value]): A list of input values.
+
+        Returns:
+            List[Value] or Value: The output of the layer.
+
+        """
         outs = [n(x) for n in self.neurons]
         return outs[0] if len(outs) == 1 else outs
-    
+
     def parameters(self):
+        """
+        Get a list of the weights and biases of all the neurons in the layer.
+
+        Returns:
+            List[Value]: A list of the weights and biases of all the neurons in the layer.
+
+        """
         return [p for neuron in self.neurons for p in neuron.parameters()]
 
-class MLP:
+    def __iter__(self):
+        self._i = 0
+        return iter(self.neurons)
+
+    def __next__(self):
+        return self.neurons[self._i]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+class MLP(Module):
+    """
+    A multilayer perceptron.
+
+    Args:
+        nin (int): The number of inputs to the MLP.
+        nouts (List[int]): A list of integers indicating the number of neurons in each layer.
+
+    Attributes:
+        layers (List[Layer]): A list of layers in the MLP.
+    """
 
     def __init__(self, nin, nouts):
         sz = [nin] + nouts
         self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
 
     def __call__(self, x):
+        """
+        Compute the output of the MLP given an input.
+
+        Args:
+            x (List[Value]): A list of input values.
+
+        Returns:
+            List[Value] or Value: The output of the MLP.
+
+        """
         for layer in self.layers:
             x = layer(x)
         return x
-    
+
     def parameters(self):
+        """
+        Get a list of the weights and biases of all the neurons in the MLP.
+
+        Returns:
+            List[Value]: A list of the weights and biases of all the neurons in the MLP.
+
+        """
         return [p for layer in self.layers for p in layer.parameters()]
+    
+    def __iter__(self):
+        self._i = 0
+        return iter(self.layers)
+
+    def __next__(self):
+        return self.layers[self._i]
+    
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
--- value.py
+++ value.py
@@ -1,21 +1,80 @@
 import math
 
 class Value:
+    """Represents a node in a computational graph for automatic differentiation.
 
-    def __init__(self, data, _parents=(), _op='', label=''):
+    Args:
+        data (float or int): The value associated with the node.
+        _children (tuple, optional): A tuple of children nodes in the graph. Defaults
+                                     to an empty tuple.
+        _op (str, optional): A string representing the operation performed on the node.
+                             Defaults to an empty string.
+        label (str, optional): A label to identify the node. Defaults to an empty string.
+
+    Attributes:x
+        data (float or int): The value associated with the node.
+        grad (float): The gradient of the node.
+        _prev (set): A set of children nodes in the graph.
+        _op (str): A string representing the operation performed on the node.
+        _backward (function): A function that computes the gradient of the node.
+
+    Methods:
+        backward(): Performs a backward pass through the computational graph and computes the
+                    gradients of each node using reverse-mode automatic differentiation.
+    Example:
+        # create a node with a value of 3.0
+        x = Value(3.0)
+
+        # create a node with a value of 4.0
+        y = Value(4.0)
+
+        # perform addition between the two nodes
+        z = x + y
+
+        # compute the gradient of the node z
+        z.backward()
+
+        # print the gradient of the node z
+        print(z.grad)"""
+
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0.0
-        
-        self._prev = set(_parents) # children is transformed from tuple to set for performance reasons
+        # children is transformed from tuple to set for performance reasons
+        self._prev = set(_children)
         self._op =  _op
-        self._backward = lambda: None # this function decides how gradient will be calculated for backpropagation
+        self._backward = lambda: None
 
-        # only for testing and visualization TODO: remove when finished
         self.label = label
 
     def backward(self):
+        """
+        Performs a backward pass through the computational graph of a neural network and computes
+        the gradients of each node using reverse-mode automatic differentiation.
+
+        Args:
+            self: The root node of the computational graph.
+
+        Returns:
+            None. The function updates the gradients of each node in the computational graph.
+
+        Example Usage:
+            Let's assume that we have a neural network represented by a computational graph, and
+            we want to compute the gradients of each node. We can do this by calling the backward()
+            method on the root node of the graph as follows:
+
+                MultiLayerPerceptron(inputs).backward()
+
+            This will update the gradients of each node in the graph, which can then be used for
+            parameter updates during the optimization process.
+        """
+
+        # initialize an empty list to store the sorted nodes
         topo = []
+        # initialize a set to keep track of visited nodes
         visited = set()
+
+        # define a helper function to perform topological sorting
         def build_topo(node):
             # topological sorting of a graph
             if node not in visited:
@@ -24,16 +83,21 @@ class Value:
                     build_topo(child)
                 topo.append(node)
 
+        # perform topological sorting starting from the root node
         build_topo(self)
 
+        # set the gradient of the output node to 1.0
         self.grad = 1.0
+
+        # compute the gradients of each node in reverse order
         for node in reversed(topo):
             node._backward()
-    
+
+    # Base binary operations
     def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other) # converts to Value() if it isnt already
+        other = other if isinstance(other, Value) else Value(other) # converts to Value() if needed
         out = Value(self.data+other.data, (self, other), '+') 
-        
+
         def _backward():
             self.grad += 1.0 * out.grad
             other.grad += 1.0 * out.grad
@@ -42,7 +106,7 @@ class Value:
         return out
 
     def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other) # converts to Value() if it isnt already
+        other = other if isinstance(other, Value) else Value(other) # converts to Value() if needed
         out = Value(self.data*other.data, (self, other), '*')
 
         def _backward():
@@ -51,7 +115,7 @@ class Value:
         out._backward = _backward
 
         return out
-    
+
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "__pow__ only support int and float exponents"
         out = Value(self.data**other, (self, ), f'**{other}')
@@ -61,7 +125,8 @@ class Value:
         out._backward = _backward
 
         return out
-    
+
+    # Unary operations
     def exp(self):
         x = self.data
         out = Value(math.exp(x), (self,), 'exp')
@@ -71,7 +136,7 @@ class Value:
         out._backward = _backward
 
         return out
-    
+
     def tanh(self):
         x = self.data
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
@@ -82,36 +147,46 @@ class Value:
         out._backward = _backward
 
         return out
-    
-    def __gt__(self, other):
-        other = other if isinstance(other, Value) else Value(other) # converts to Value() if it isnt already
-        return self.data > other.data
-    
+
     def __abs__(self):
         return -self if self.data < 0 else self
-    
+
     def __neg__(self):
         return self * -1
-    
+
+    # More Binary operations
     def __rmul__(self, other):
         return  self * other
-    
+
     def __radd__(self, other):
         return self + other
-    
+
     def __sub__(self, other):
-        # other = other if isinstance(other, Value) else Value(other) # converts to Value() if it isnt already
         return self + (-other)
-    
+
     def __rsub__(self, other):
         return self + (-other)
 
     def __truediv__(self, other):
-        # other = other if isinstance(other, Value) else Value(other) # converts to Value() if it isnt already
         return  self * (other**-1)
-    
+
     def __rtruediv__(self,other):
         return  self * (other**-1)
+
+    # Binary comparison operations
+    def __gt__(self, other):
+        other = other if isinstance(other, Value) else Value(other) # converts to Value() if needed
+        return self.data > other.data
+
+    def __ge__(self, other):
+        other = other if isinstance(other, Value) else Value(other) # converts to Value() if needed
+        return self.data >= other.data
+
+    def __lt__(self, other):
+        return not (self >= other)
+
+    def __le__(self, other):
+        return not ( self > other)
 
     def __repr__(self):
         return f'{self.label}: Value(data={self.data}, grad={self.grad})'
